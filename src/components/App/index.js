1c23ecbf0c54ba91c2daa8eb6142c92c68dcd8ae
unknown <NeUNekin@hotmail.com>
2020-04-29 11:31:22 +0700

    create homepage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,9 +5,8 @@ import Login from '../Login'
 import Register from '../Register'
 import Dashboard from '../Dashboard'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { CssBaseline } from '@material-ui/core'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 const theme = createMuiTheme()
 
 export default function App() {
